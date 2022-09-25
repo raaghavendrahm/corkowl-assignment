29f8bbffdba00443f8ddd59956545315cf58644d
raaghavendrahm <raaghavendrahm@gmail.com>
2022-09-26 02:25:15 +0530

    Add rightPane component.

--- src/components/body/bodyComp.vue
+++ src/components/body/bodyComp.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="bodyComp">
     <left-pane />
+    <right-pane />
   </div>
 </template>
 
 <script>
   import leftPane from "../leftPane/leftPane.vue";
+  import rightPane from "../rightPane/rightPane.vue";
 
   export default {
     name: "bodyComp",
     components: {
-      leftPane
+      leftPane,
+      rightPane
     }
   }
 </script>
---
+++ src/components/rightPane/rightPane.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="right-pane">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "rightPane",
+  }
+</script>
+
+<style>
+  @import "./rightPane.css";
+</style>
