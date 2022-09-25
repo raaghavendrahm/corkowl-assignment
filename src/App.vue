be7896b68a20fa31a105c4330758f90aab3ee161
raaghavendrahm <raaghavendrahm@gmail.com>
2022-09-26 01:48:04 +0530

    Install font awesome, import in main.js file, and test for icons.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     CORKOWL ASSIGNMENT
+    <font-awesome-icon :icon="['fas', 'search']" />
   </div>
 </template>
 
@@ -9,7 +10,7 @@
 export default {
   name: 'App',
   components: {
-    
+
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+/* import specific icons */
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+// import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
+// import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
+/* add icons to the library */
+library.add(fas, far);
+
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 new Vue({
