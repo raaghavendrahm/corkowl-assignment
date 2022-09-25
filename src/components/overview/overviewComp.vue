2a241a77e58f775e6239721d20d9fc9b151083ce
raaghavendrahm <raaghavendrahm@gmail.com>
2022-09-26 02:29:47 +0530

    Add overview component.

---
+++ src/components/overview/overviewComp.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="overview">
+    <p>Overview</p>
+      <button class="add">
+        <div class="icon">
+          <font-awesome-icon :icon="['fas', 'plus-circle']" />
+        </div>
+        <p>ADD NEW WINE</p>
+      </button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "overviewComp",
+  }
+</script>
+
+<style>
+  @import "./overviewComp.css";
+</style>
--- src/components/rightPane/rightPane.vue
+++ src/components/rightPane/rightPane.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="right-pane">
+    <overview-comp />
   </div>
 </template>
 
 <script>
+  import overviewComp from "../overview/overviewComp.vue";
+
   export default {
     name: "rightPane",
+    components: {
+      overviewComp
+    }
   }
 </script>
 
