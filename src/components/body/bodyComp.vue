1cb2608b6de9569f059b8004483f8c051f46b9c3
raaghavendrahm <raaghavendrahm@gmail.com>
2022-09-26 02:21:19 +0530

    Add leftPane component.

--- src/components/body/bodyComp.vue
+++ src/components/body/bodyComp.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="bodyComp">
+    <left-pane />
   </div>
 </template>
 
 <script>
+  import leftPane from "../leftPane/leftPane.vue";
+
   export default {
     name: "bodyComp",
+    components: {
+      leftPane
+    }
   }
 </script>
 
---
+++ src/components/leftPane/leftPane.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="leftPane">
+    <div class="leftPane-wrapper">
+      <div class="leftPane-wrapper-flex-container">
+        <div class="flex-item">
+          <div class="icon">
+            <font-awesome-icon :icon="['fas', 'desktop']" />
+          </div>
+          <p>Dashboard</p>
+      </div>
+
+        <div class="flex-item">
+          <div class="icon">
+            <font-awesome-icon :icon="['fas', 'layer-group']" />
+          </div>
+          <p>Inventory</p>
+      </div>
+        <div class="flex-item">
+          <div class="icon">
+            <font-awesome-icon :icon="['fas', 'shopping-bag']" />
+          </div>
+          <p>Orders</p>
+      </div>
+        <div class="flex-item">
+          <div class="icon">
+            <font-awesome-icon :icon="['fas', 'user-friends']" />
+          </div>
+          <p>Customers</p>
+      </div>
+        <div class="flex-item">
+          <div class="icon">
+            <font-awesome-icon :icon="['far', 'file-alt']" />
+          </div>
+          <p>Reports</p>
+      </div>
+        <div class="flex-item">
+          <div class="icon">
+            <font-awesome-icon :icon="['fas', 'dharmachakra']" />
+          </div>
+          <p>Settings</p>
+      </div>
+
+      <p class="footer-heading">CorkOwl</p>
+      <p class="footer-sub-heading">
+        &copy; 2022 CorkOwl, All Rights Reserved.
+      </p>
+
+      <div class="left-arrow">
+        <font-awesome-icon :icon="['fas', 'arrow-circle-left']" />
+      </div>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "leftPane",
+    // data() {
+    //   return {
+    //     dataFromAPI: {
+    //       tabs: ['Dashboard', 'Inventory', 'Orders', 'Customers', 'Reports', 'Settings'],
+    //     }
+    //   }
+    // }
+  }
+</script>
+
+<style>
+  @import "./leftPane.css";
+</style>
